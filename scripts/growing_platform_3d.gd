49059996588f5c5eaf3b2b20d6bdcd9025c8eebb
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-03-16 13:02:59 -0500

    Added Direction to Platform

--- scripts/growing_platform_3d.gd
+++ scripts/growing_platform_3d.gd
@@ -1,14 +1,24 @@
-extends CSGBox3D
+@tool
+class_name GrowingPlatform3D extends CSGBox3D
 
-# TODO: Direction, Signal (Start Moving), Timer, Speed (Velocity)
 @export var move_time := 50.0
-@export var move_distance := 100
 @export var movement_delay := 5.0 
+@export var final_relative_pos := Vector3(0,5,0)
+
 @onready var timer = $Timer
+@onready var raycast3D = $RayCast3D
+
+func _ready():
+	raycast3D.top_level = true
+	
+func _process(delta):
+	if not raycast3D.target_position == final_relative_pos:
+		raycast3D.target_position = final_relative_pos
+	
 
 func _on_timer_timeout():
 	var tween = create_tween()
-	tween.tween_property(self,"global_position",Vector3.UP * move_distance + global_position,move_time)
+	tween.tween_property(self,"global_position", raycast3D.target_position + global_position,move_time)
 
 func start_timer():
 	timer.wait_time = movement_delay
