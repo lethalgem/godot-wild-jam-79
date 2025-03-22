1a092bcdcb612522558c2ffcaea845c6bba3b6ba
lethalgem <iwc123@gmail.com>
2025-03-22 10:51:24 -0400

    Exit menu when esc pressed and add 'resume'

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -6,6 +6,8 @@ class_name MainGame extends Node3D
 @onready var return_to_camera_anchor_rotation := player.camera_anchor.rotation
 @onready var return_to_camera_3D_fov := player.camera_3D.fov
 
+var player_paused_game := false
+
 func _ready() -> void:
 	# Listen to all sections to know when the player has entered
 	for possible_child_section in get_children():
@@ -19,19 +21,20 @@ func _ready() -> void:
 						possible_death_plane_child.connect("player_entered", respawn.bind(section))
 				)
 				
-	show_start_menu(false)
+	show_start_menu()
 	
 ## Ensures that the music is only interrupted when we respawn and not before
 func respawn(section: PlatformingSection):
 	section.reset_and_respawn()
 	$AudioStreamPlayer.play(section.song_start)
 
-func show_start_menu(should_animate : bool):
-	start_menu.show()
+func show_start_menu():
 	player.camera_anchor.pause_fov_change = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
-	if should_animate:
+	start_menu.show_menu(player_paused_game)
+	
+	if player_paused_game:
 		var tween = create_tween()
 		tween.parallel().tween_property(player.camera_anchor, "rotation", Vector3(-0.4,-3.02,0.0),0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 		tween.parallel().tween_property(player.camera_3D, "fov", 35, 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
@@ -48,10 +51,16 @@ func hide_start_menu():
 	tween.parallel().tween_property(player.camera_3D, "fov", return_to_camera_3D_fov, 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
 	player.camera_anchor.pause_fov_change = false
+	
 func _on_start_menu_canvas_start_button_pressed():
 	hide_start_menu()
 
-func _input(event):
-	if event.is_action_released("pause"):
-		show_start_menu(true)
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		player_paused_game = !player_paused_game
+		if player_paused_game:
+			show_start_menu()
+		else:
+			hide_start_menu()
+	
 	
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,5 +2,17 @@ class_name StartMenu extends CanvasLayer
 
 signal start_button_pressed
 
+@export var start_button: Button
+
+@onready var original_start_button_text := start_button.text
+
+func show_menu(is_player_pause: bool):
+	show()
+	
+	if is_player_pause:
+		start_button.text = "Resume Game"
+	else:
+		start_button.text = original_start_button_text
+
 func _on_start_game_pressed():
 	start_button_pressed.emit()
